eedbb0f170426f2cc1e73bc15b902332809bedbf
Rajender <rthakur.dev@gmail.com>
2017-03-30 21:13:47 +0530

    New temp updated

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+$brands =
+[
+  ['name' => 'Alfa Romeo', 'logo' => 'image/carlogo.jpg', 'types' => [ 'Type1', 'Type1' ]],
+  ['name' => 'Audi', 'logo' => 'image/carlogo.jpg', 'types' => [ 'Type1', 'Type1' ]],
+  ['name' => 'Bentley', 'logo' => 'image/carlogo.jpg', 'types' => ['section1', 'section2']],
+  ['name' => 'Bentley', 'logo' => 'image/carlogo.jpg', 'types' => []]
+];
+
+$pages =
+[
+  ['name' => 'about us'],
+  ['name' => 'login' ],
+  ['name' => 'register']
+];
+
+$sliderImages =
+[
+   ['image'=>'image/alloy-wheels-rims-tsw-amaroo-5-lug-rear-brushed-silver-std-250.jpg'],
+   ['image'=>'image/alloy-wheels-rims-tsw-amaroo-5-lug-matte-black-std-250.jpg'],
+   ['image'=>'image/alloy-wheels-rims-tsw-ascent-5-lug-matte-gunmetal-gloss-black-face-std-250.jpg']
+];
+
+
+
+?>
