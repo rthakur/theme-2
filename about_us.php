895a03c761c2d546637f8479158f817371bc212e
Rajender <rthakur.dev@gmail.com>
2017-04-04 20:34:25 +0530

    About us page addded

---
+++ about_us.php
@@ -0,0 +1,62 @@
+<?php include('includes/header.php') ?>
+<div class="contact-container">
+    <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="contact-label">About Us
+                <small style="text-transform: none;">It's Nice to Meet You!</small>
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+        </div>
+    </div>
+    <!-- Team Members Row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Our Team</h2>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+            <img class="img-circle img-responsive img-center" src="image/about_us.png" alt="">
+            <h3>Lorem Ipsum
+                <small>Job Title</small>
+            </h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+            <img class="img-circle img-responsive img-center" src="image/about_us.png" alt="">
+            <h3>Lorem Ipsum
+                <small>Job Title</small>
+            </h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+            <img class="img-circle img-responsive img-center" src="image/about_us.png" alt="">
+            <h3>Lorem Ipsum
+                <small>Job Title</small>
+            </h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+            <img class="img-circle img-responsive img-center" src="image/about_us.png" alt="">
+            <h3>Lorem Ipsum
+                <small>Job Title</small>
+            </h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+            <img class="img-circle img-responsive img-center" src="image/about_us.png" alt="">
+            <h3>Lorem Ipsum
+                <small>Job Title</small>
+            </h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+            <img class="img-circle img-responsive img-center" src="image/about_us.png" alt="">
+            <h3>Lorem Ipsum
+                <small>Job Title</small>
+            </h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+    </div>
+</div>
+</div>
+<?php include('includes/footer.php') ?>
